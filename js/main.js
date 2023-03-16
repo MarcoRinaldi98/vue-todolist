db27f19bbdc29b19fe6ad924f7177e59e3b429b5
Marco Rinaldi <marcorinaldi98@libero.it>
2023-03-16 14:05:47 +0100

    MILESTONE 1

--- js/main.js
+++ js/main.js
@@ -7,10 +7,28 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+            todolist: [
+                {
+                    text: 'Task 1',
+                    done: false
+                },
+                {
+                    text: 'Task 2',
+                    done: false
+                },
+                {
+                    text: 'Task 3',
+                    done: true
+                },
+                {
+                    text: 'Task 4',
+                    done: false
+                },
+                {
+                    text: 'Task 5',
+                    done: true
+                }               
+            ]
         }           
-    }, 
-    methods: {
-        
     }
 }).mount('#app')
